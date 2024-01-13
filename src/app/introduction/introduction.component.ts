2a66e0757f3e26e6cca120622946bc868caf3815
Mayank Madan <my905874@dal.ca>
2024-01-13 16:08:34 -0400

    Added in-transitions using snimations and gsap

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-introduction',
   templateUrl: './introduction.component.html',
   styleUrls: ['./introduction.component.scss']
 })
-export class IntroductionComponent {
+
+export class IntroductionComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    this.animateText();
+  }
+
+  animateText() {
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+    tl.to('.hide-elem', { y: '0%', duration: 1, delay: 3, stagger: 0.25 });
+  }
 
 }
